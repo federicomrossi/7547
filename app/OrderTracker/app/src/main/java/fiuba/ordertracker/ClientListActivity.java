04b3b16e1ba75c4d451001a30e32217656ccd74e
Federico Rossi <federicomrossi@gmail.com>
2016-03-29 20:41:02 -0300

    Added a RecyclerView to show a clients list as main activity. The data is hardcoded.

--- app/OrderTracker/app/src/main/java/fiuba/ordertracker/ClientListActivity.java
+++ app/OrderTracker/app/src/main/java/fiuba/ordertracker/ClientListActivity.java
@@ -1,14 +1,24 @@
 package fiuba.ordertracker;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.AttributeSet;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ClientListActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private RecyclerView recyclerView;
+    private ClientListAdapter clientListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +29,12 @@ public class ClientListActivity extends AppCompatActivity {
         toolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setSubtitle(getString(R.string.activity_client_list));
+
+        // Clients list
+        recyclerView = (RecyclerView) findViewById(R.id.clientsList);
+        clientListAdapter = new ClientListAdapter(this, getData());
+        recyclerView.setAdapter(clientListAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
     @Override
@@ -33,5 +49,28 @@ public class ClientListActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    /**
+     * TEMP!!!
+     */
+
+    public static List<ClientModel> getData() {
+        List<ClientModel> data = new ArrayList<>();
+
+        String[] names = {"Sabrina Campa", "Pablo Ascarza", "Ezequier Reyes", "Federico Rossi"};
+        String[] addresses = {"Av. Mitre 123, Bs. As.", "Av. Mitre 123, Bs. As.", "Av. Mitre 123, Bs. As.", "Av. Mitre 123, Bs. As."};
+        String[] distances = {"1 km", "2 km", "3 km", "4 km"};
+
+        for(int i = 0; i < names.length; i++) {
+            ClientModel c = new ClientModel();
+            c.name = names[i];
+            c.address = addresses[i];
+            c.distance = distances[i];
+            data.add(c);
+        }
+
+        return data;
     }
 }
