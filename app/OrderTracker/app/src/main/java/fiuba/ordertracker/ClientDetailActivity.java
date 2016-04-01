7929253f0053ab6556d18f62634e32b0fd15b4c8
scampa <sabrina_campa@yahoo.com.ar>
2016-04-01 20:26:07 -0300

    Listener that handles clicks on clients and Toast to show message that there are no clients in the app

--- app/OrderTracker/app/src/main/java/fiuba/ordertracker/ClientDetailActivity.java
+++ app/OrderTracker/app/src/main/java/fiuba/ordertracker/ClientDetailActivity.java
@@ -1,7 +1,9 @@
 package fiuba.ordertracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ClientDetailActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class ClientDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_detail);
+
+        Intent i = getIntent();
+
+        TextView name = (TextView) findViewById(R.id.client_name);
+        TextView address = (TextView) findViewById(R.id.client_address);
+        TextView distance = (TextView) findViewById(R.id.client_distance);
+
+        // Set data from intent
+        name.setText("Name: " + i.getStringExtra("name"));
+        address.setText("Address: " + i.getStringExtra("address"));
+        distance.setText("Distance: " + i.getStringExtra("distance"));
     }
 }
--- app/OrderTracker/app/src/main/java/fiuba/ordertracker/ClientListAdapter.java
+++ app/OrderTracker/app/src/main/java/fiuba/ordertracker/ClientListAdapter.java
@@ -1,11 +1,13 @@
 package fiuba.ordertracker;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Collections;
 import java.util.List;
@@ -23,6 +25,11 @@ public class ClientListAdapter extends RecyclerView.Adapter<ClientListAdapter.My
     public ClientListAdapter(Context context, List<Client> data) {
         inflater = LayoutInflater.from(context);
         this.data = data;
+
+        // Show message notifying there are no clients
+        if (this.data.size() == 0){
+            Toast.makeText(context, "No hay clientes en el sistema", Toast.LENGTH_LONG).show();
+        }
     }
 
     @Override
@@ -39,6 +46,21 @@ public class ClientListAdapter extends RecyclerView.Adapter<ClientListAdapter.My
         holder.name.setText(current.getApenom());
         holder.address.setText(current.getDireccion());
         holder.distance.setText(current.getId());
+
+        // Set listener to manage clicks on items from the RecyclerView
+        holder.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                System.out.println("*********** Click on item ***********");
+
+                Intent intent = new Intent(view.getContext(), ClientDetailActivity.class);
+                intent.putExtra("name", data.get(position).getApenom());
+                intent.putExtra("address", data.get(position).getDireccion());
+                intent.putExtra("distance", data.get(position).getTelefono());
+
+                view.getContext().startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -46,11 +68,11 @@ public class ClientListAdapter extends RecyclerView.Adapter<ClientListAdapter.My
         return this.data.size();
     }
 
-    class MyViewHolder extends RecyclerView.ViewHolder {
-
+    class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         TextView name;
         TextView address;
         TextView distance;
+        private OnItemClickListener clickListener;
 
         public MyViewHolder(View itemView) {
             super(itemView);
@@ -58,6 +80,32 @@ public class ClientListAdapter extends RecyclerView.Adapter<ClientListAdapter.My
             name = (TextView) itemView.findViewById(R.id.client_list_row_name);
             address = (TextView) itemView.findViewById(R.id.client_list_row_address);
             distance = (TextView) itemView.findViewById(R.id.client_list_row_distance);
+
+            // Set listener to the item view
+            itemView.setOnClickListener(this);
+        }
+
+        public void setOnItemClickListener(OnItemClickListener clickListener) {
+            this.clickListener = clickListener;
+        }
+
+        @Override
+        public void onClick(View view) {
+            clickListener.onItemClick(view, getAdapterPosition()); // or getLayoutPosition() ??
         }
     }
+
+    /**
+     * Interface for handling clicks
+     */
+    private interface OnItemClickListener {
+        /**
+         * Called when the view is clicked
+         * @param view that is clicked
+         * @param position of the clicked item
+         */
+        public void onItemClick(View view, int position);
+
+    }
+
 }
