b1eb47b2b732556d95be4a8eafb740dd195df300
Federico Rossi <federicomrossi@gmail.com>
2016-04-01 23:44:56 -0300

    Created the view for the products categories.

--- app/OrderTracker/app/src/main/java/fiuba/ordertracker/ProductCategoryListActivity.java
+++ app/OrderTracker/app/src/main/java/fiuba/ordertracker/ProductCategoryListActivity.java
@@ -2,14 +2,114 @@ package fiuba.ordertracker;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.util.List;
+
+import fiuba.ordertracker.pojo.Client;
+import fiuba.ordertracker.services.ClientService;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * Created by scampa on 1/4/2016.
  */
 public class ProductCategoryListActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+    private RecyclerView recyclerView;
+    private ClientListAdapter clientListAdapter;
+    private ProgressBar progressBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_category_list);
+
+        // Toolbar
+        toolbar = (Toolbar) findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setSubtitle(getString(R.string.activity_client_list));
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.VISIBLE);
+
+        setProgressBarIndeterminateVisibility(true);
+
+        // Clients list
+        recyclerView = (RecyclerView) findViewById(R.id.clientsList);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        ClientService cs = ClientService.getInstance();
+
+        // Create a call instance for looking up Retrofit contributors.
+        Call<List<Client>> call = cs.clients.Clients(null,null,null,null);
+
+        final ProductCategoryListActivity self_ = this;
+        call.enqueue(new Callback<List<Client>>() {
+            @Override
+            public void onResponse(Call<List<Client>> call, Response<List<Client>> response) {
+                // Get result Repo from response.body()
+                List<Client> listClients = response.body();
+                clientListAdapter = new ClientListAdapter(self_, listClients);
+                progressBar.setVisibility(View.GONE);
+
+                if(listClients.size() == 0) {
+                    TextView textNoClients = (TextView) findViewById(R.id.text_no_clients);
+                    textNoClients.setVisibility(View.VISIBLE);
+                }
+
+                recyclerView.setAdapter(clientListAdapter);
+            }
+
+            @Override
+            public void onFailure(Call<List<Client>> call, Throwable t) {
+                //Aca tenemos que agregar el msj de error a mostrar... puto el que lee
+            }
+        });
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        /*final SearchView searchView = (SearchView) MenuItemCompat.getActionView(menu.findItem(R.id.action_search));
+        SearchManager searchManager = (SearchManager) getSystemService(SEARCH_SERVICE);
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));*/
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        /*switch(id) {
+
+            case R.id.action_filter:
+                toolbar_filter();
+                break;
+
+            case R.id.action_search:
+                break;
+        }*/
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void toolbar_filter() {
+
+    }
+
+
 }
