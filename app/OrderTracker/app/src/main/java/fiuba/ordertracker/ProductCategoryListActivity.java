7d3a00d349c7887bf5d4a37c8f14e05735211972
scampa <sabrina_campa@yahoo.com.ar>
2016-04-01 22:32:32 -0300

    Add button in activity_client_detail.xml to go to the shopping cart/list of categories

---
+++ app/OrderTracker/app/src/main/java/fiuba/ordertracker/ProductCategoryListActivity.java
@@ -0,0 +1,15 @@
+package fiuba.ordertracker;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by scampa on 1/4/2016.
+ */
+public class ProductCategoryListActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_category_list);
+    }
+}
