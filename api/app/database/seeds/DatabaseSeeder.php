5858ac2fec7a6ce9d4b551a334d66186e899e9b2
Ezequiel <ezequielmreyes@gmail.com>
2016-04-02 16:33:25 -0300

    US-M-10-T-API-2: changed client and product table values. Added estado field.

--- api/app/database/seeds/DatabaseSeeder.php
+++ api/app/database/seeds/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder {
 		$this->call('CategoriesTableSeeder');
 		$this->call('ClientUpdateTableSeeder');
 		$this->call('ProductsUrlImagesTableSeeder');
+		$this->call('UpdateClientsAndProductsTableSeeder');
 		// $this->call('UserTableSeeder');
 	}
 
