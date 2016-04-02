ec0113abfeb478413bc03205db1fa892d9b85b4e
Federico Rossi <federicomrossi@gmail.com>
2016-04-02 17:10:47 -0300

    Changed the look & feel of the client layout.

--- app/OrderTracker/app/src/main/java/fiuba/ordertracker/ClientDetailActivity.java
+++ app/OrderTracker/app/src/main/java/fiuba/ordertracker/ClientDetailActivity.java
@@ -1,8 +1,10 @@
 package fiuba.ordertracker;
 
 import android.content.Intent;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
 
@@ -13,16 +15,21 @@ public class ClientDetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_client_detail);
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.client_detail_toolbar);
+        setSupportActionBar(toolbar);
+        CollapsingToolbarLayout collapsingToolbar = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
+
         Intent i = getIntent();
 
-        TextView name = (TextView) findViewById(R.id.client_name);
+        //TextView name = (TextView) findViewById(R.id.client_name);
         TextView address = (TextView) findViewById(R.id.client_address);
         TextView distance = (TextView) findViewById(R.id.client_distance);
 
         // Set data from intent
-        name.setText("Name: " + i.getStringExtra("name"));
-        address.setText("Address: " + i.getStringExtra("address"));
-        distance.setText("Distance: " + i.getStringExtra("distance"));
+        collapsingToolbar.setTitle(i.getStringExtra("name"));
+        //name.setText(i.getStringExtra("name"));
+        address.setText(i.getStringExtra("address"));
+        distance.setText(i.getStringExtra("distance"));
     }
 
     //Call when the user clicks the button
@@ -31,5 +38,4 @@ public class ClientDetailActivity extends AppCompatActivity {
         view.getContext().startActivity(intent);
 
     }
-
 }
