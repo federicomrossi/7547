cec49933786b96b9bd0a242b60b5b083bd94ac46
Ezequiel <ezequielmreyes@gmail.com>
2016-04-05 17:55:37 -0300

    Changed url server.

--- app/OrderTracker/app/src/main/java/fiuba/ordertracker/helpers/Constants.java
+++ app/OrderTracker/app/src/main/java/fiuba/ordertracker/helpers/Constants.java
@@ -6,7 +6,7 @@ package fiuba.ordertracker.helpers;
 public final class Constants {
     public static final String UNIT = "K";
     public static final String COMPLETE_UNIT = "Km";
-    public static final String URL_SERVER = "http://ec2-54-207-103-117.sa-east-1.compute.amazonaws.com/";
+    public static final String URL_SERVER = "http://ec2-52-33-59-48.us-west-2.compute.amazonaws.com";
     public static final Double FIUBA_LATITUDE = 34.6175246;
     public static final Double FIUBA_LONGITUDE = -58.3705057;
 }
