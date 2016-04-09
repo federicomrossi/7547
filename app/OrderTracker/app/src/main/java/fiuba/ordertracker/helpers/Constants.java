ce0241ba095d992c919091a5cf6e2e805a2d8925
Ezequiel <ezequielmreyes@gmail.com>
2016-04-09 01:19:37 -0300

    12-1-guifiltros: added filters and color to search/edit view.

--- app/OrderTracker/app/src/main/java/fiuba/ordertracker/helpers/Constants.java
+++ app/OrderTracker/app/src/main/java/fiuba/ordertracker/helpers/Constants.java
@@ -1,5 +1,7 @@
 package fiuba.ordertracker.helpers;
 
+import android.graphics.Color;
+
 /**
  * Created by ezequiel on 02/04/16.
  */
@@ -9,4 +11,6 @@ public final class Constants {
     public static final String URL_SERVER = "http://ec2-52-33-59-48.us-west-2.compute.amazonaws.com";
     public static final Double FIUBA_LATITUDE = 34.6175246;
     public static final Double FIUBA_LONGITUDE = -58.3705057;
+    public static final Integer COLOR_TEXT_FILTER = Color.BLACK;
+    public static final Integer COLOR_HINT_FILTER = Color.BLACK;
 }
