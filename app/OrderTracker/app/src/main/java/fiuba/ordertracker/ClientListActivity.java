f36a8cb4850910cac418bf646e19c40e3d12d8ff
Federico Rossi <federicomrossi@gmail.com>
2016-04-24 02:05:39 -0300

    Added tabs to the clients list.

--- app/OrderTracker/app/src/main/java/fiuba/ordertracker/ClientListActivity.java
+++ app/OrderTracker/app/src/main/java/fiuba/ordertracker/ClientListActivity.java
@@ -1,7 +1,13 @@
 package fiuba.ordertracker;
 
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -19,6 +25,7 @@ import android.widget.SearchView;
 import android.widget.TextView;
 import android.content.Intent;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import fiuba.ordertracker.helpers.FiltersHelper;
@@ -30,9 +37,13 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class ClientListActivity extends AppCompatActivity {
+public class ClientListActivity extends AppCompatActivity
+        implements ClientListFragment.OnFragmentInteractionListener {
 
     private Toolbar toolbar;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
     private RecyclerView recyclerView;
     private ClientListAdapter clientListAdapter;
     private ProgressBar progressBar;
@@ -48,6 +59,16 @@ public class ClientListActivity extends AppCompatActivity {
         toolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setSubtitle(getString(R.string.activity_client_list));
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+        tabLayout.getTabAt(7).setIcon(R.drawable.ic_call_split_white_24dp);
+
+/*
         final SearchView razonFilterView = (SearchView)findViewById(R.id.searchView);
         final EditText clientCodeFilterView = (EditText)findViewById(R.id.editText_client_code);
         Fonts.changeSearchViewTextColorBlack(clientCodeFilterView);
@@ -132,7 +153,54 @@ public class ClientListActivity extends AppCompatActivity {
 
             }
         });
+*/
+    }
 
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new ClientListFragment(), "D");
+        adapter.addFragment(new ClientListFragment(), "L");
+        adapter.addFragment(new ClientListFragment(), "M");
+        adapter.addFragment(new ClientListFragment(), "M");
+        adapter.addFragment(new ClientListFragment(), "J");
+        adapter.addFragment(new ClientListFragment(), "V");
+        adapter.addFragment(new ClientListFragment(), "S");
+        adapter.addFragment(new ClientListFragment(), "");
+        viewPager.setAdapter(adapter);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
     }
 
     @Override
@@ -156,7 +224,7 @@ public class ClientListActivity extends AppCompatActivity {
 
     public void onClickShowHideFilters(View view) {
 
-        LinearLayout button_filter = (LinearLayout) findViewById(R.id.filters_container);
+        /*LinearLayout button_filter = (LinearLayout) findViewById(R.id.filters_container);
 
         if(button_filter.getVisibility() == View.GONE)
             button_filter.setVisibility(View.VISIBLE);
@@ -168,7 +236,7 @@ public class ClientListActivity extends AppCompatActivity {
 
             EditText editText_client_code = (EditText) findViewById(R.id.editText_client_code);
             editText_client_code.clearFocus();
-        }
+        }*/
     }
 
     // When the user clicks the "Ver carrito" button
@@ -178,12 +246,12 @@ public class ClientListActivity extends AppCompatActivity {
 
     public void setRecycler()
     {
-        String codeFilter = ((EditText) findViewById(R.id.editText_client_code)).getText().toString();
+        /*String codeFilter = ((EditText) findViewById(R.id.editText_client_code)).getText().toString();
         String nameFilter = ((SearchView) findViewById(R.id.searchView)).getQuery().toString();
         List<Client> listFiltered = FiltersHelper.filterClientsBySocialReason(clientListAdapter.getOriginalData(), nameFilter);
         listFiltered = FiltersHelper.filterClientsByCode(listFiltered, codeFilter);
         clientListAdapter.setData(listFiltered);
-        recyclerView.setAdapter(clientListAdapter);
+        recyclerView.setAdapter(clientListAdapter);*/
     }
 
     // Call when the user clicks the go map button
