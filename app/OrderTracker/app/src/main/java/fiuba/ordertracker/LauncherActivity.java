c771a9e8759da4eaf2877dacaf51a04b9b3981e1
scampa <sabrina_campa@yahoo.com.ar>
2016-05-29 00:10:25 -0300

    FEDE's fix

--- app/OrderTracker/app/src/main/java/fiuba/ordertracker/LauncherActivity.java
+++ app/OrderTracker/app/src/main/java/fiuba/ordertracker/LauncherActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class LauncherActivity extends AppCompatActivity {
 
@@ -11,16 +12,22 @@ public class LauncherActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
 
-        SharedPreferences pref = getApplicationContext().getSharedPreferences("OrderTrackerPref", 0);
+        /*SharedPreferences pref = getApplicationContext().getSharedPreferences("OrderTrackerPref", 0);
         int idVendedor = pref.getInt("id", 0);
+
         if(idVendedor == 0){
             Intent intent = new Intent(this, LoginActivity.class);
             startActivity(intent);
         }else{
             Intent intent = new Intent(this, ClientListActivity.class);
             startActivity(intent);
-        }
-        finish();
+        }*/
+        /*Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);*/
+        //finish();
     }
 }
